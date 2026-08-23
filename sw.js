a489677318a24e4b2bd7799b375dc960b11139e2
Sameiflovi <bessie.viberossa@gmail.com>
2026-08-23 18:45:39 -0400

    se soluciono el bug del div de el espacio inesesario , ahora esta bien purgadito tambien se puso en orden el tema de catalogo de las imagenes que seguian siendo utilizadas por png no el nuevo webp se soluciono el problema del modo oscuro obligado por los navegadores MIUI, ahora se fuerza a que muestren el navegador en modo claro para no afectar a los estilos principales y que los navegadores no pongan sus estilos raros

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // ======================================================
 
 // Cambia esta versión cuando publiques una actualización importante
-const VERSION = "1.0.3";
+const VERSION = "1.0.4";
 
 // Nombre del caché principal
 const CACHE_NAME = `dulcearte-${VERSION}`;
