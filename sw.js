3f1226fb6279e108dae7a0fb4a49b78993b48771
Sameiflovi <bessie.viberossa@gmail.com>
2026-09-01 14:50:58 -0400

    correccion de error de catalogo.html, una comilla perjudicaba la explicacion de una descripcion  y se subieron las versiones

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // ======================================================
 
 // Cambia esta versión cuando publiques una actualización importante
-const VERSION = "1.0.7";
+const VERSION = "1.0.8";
 
 // Nombre del caché principal
 const CACHE_NAME = `dulcearte-${VERSION}`;
